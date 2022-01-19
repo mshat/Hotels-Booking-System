f87663dab08f169af962f7b8ca882a0de7b16a6d
Maksim Shatokhin <max.sh99@mail.ru>
2022-01-19 20:21:24 +0300

    Update imports and requirements

--- services/payment/payment/views.py
+++ services/payment/payment/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from rest_framework.response import Response
-from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
